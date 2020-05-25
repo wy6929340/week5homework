3d5ae54c4205be0d3470935c69f0b6b1340c6b6f
wy6929340 <ly00558@hotmail.com>
2020-05-25 20:41:37 +0800

    Initial commit

---
+++ gulpfile.js/envOptions.js
@@ -0,0 +1,66 @@
+const srcPath = './app';
+const distPath = './dist';
+const nodePath = './node_modules';
+
+let envOptions = {
+  string: 'env',
+  default: {
+    env: 'dev',
+  },
+  conyFile: {
+    src: [
+      `${srcPath}/**/*`,
+      `!${srcPath}/assets/js/**/*.js`,
+      `!${srcPath}/assets/scss/**/*.scss`,
+      `!${srcPath}/assets/scss/**/*.sass`,
+      `!${srcPath}/assets/scss/**/*.css`,
+      `!${srcPath}/**/*.ejs`,
+      `!${srcPath}/**/*.html`,
+    ],
+    path: distPath,
+  },
+  html: {
+    src: [
+      `${srcPath}/**/*.html`,
+    ],
+    ejsSrc: [
+      `${srcPath}/**/*.ejs`,
+    ],
+    path: distPath,
+  },
+  style: {
+    src: [
+      `${srcPath}/assets/scss/**/*.scss`,
+      `${srcPath}/assets/scss/**/*.sass`,
+      `${srcPath}/assets/scss/**/*.css`,
+    ],
+    path: `${distPath}/assets/css`,
+  },
+  javascript: {
+    src: [
+      `${srcPath}/assets/js/**/*.js`
+    ],
+    concat: 'all.js',
+    path: `${distPath}/assets/js`,
+  },
+  vendors: {
+    src: [
+      `${nodePath}/jquery/dist/**/jquery.min.js`,
+    ],
+    concat: 'vendors.js',
+    path: `${distPath}/assets/js`,
+  },
+  img: {
+    src: [
+      `${srcPath}/assets/images/**/*`,
+    ],
+  },
+
+  clean: {
+    src: distPath,
+  },
+  browserDir: distPath,
+  deploySrc: `${distPath}/**/*`,
+};
+
+exports.envOptions = envOptions;
